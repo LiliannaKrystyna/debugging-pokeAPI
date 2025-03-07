e8034f75ac04acb7b7abc1bb500eeebde2806e1d
Lilianna Guderska <lilianna.guderska@renault.com>
2025-03-07 13:06:00 +0100

    reparation

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-let button, image, pokeNumber, pokeName;
+let button = document.getElementById("button");
+let image = document.getElementById("image");
+let pokeNumber = document.getElementById("number");
+let pokeName = document.getElementById("name");
 
-document.addEventListener('DOMContentLoaded', () => {
-    button = document.getElementById("button");
-    image = document.getElementById("image");
-    pokeNumber = document.getElementById("number");
-    pokeName = document.getElementById("name");
-});
-
-let requestString = `https://pokeapi.co/api/v2/pokemon/${pokeName}/${randomNumber}`;
 
 const changePokemon = async () => {
+
     let randomNumber = Math.ceil(Math.random() * 150 + 1);
 
+    let requestString = `https://pokeapi.co/api/v2/pokemon/${randomNumber}`;
 
-    let data = fetch(requestString);
 
+    let data = await fetch(requestString);
+    console.log(data);
 
-    let response = await data.json(string);
-
+    let response = await data.json();
+    console.log(response);
 
     image.src = response.sprites.front_default;
     pokeNumber.textContent = `#${response.id}`;
@@ -25,6 +23,4 @@ const changePokemon = async () => {
 };
 
 changePokemon();
-element.attachEvent("onclick", function() {
-
-});
+button.addEventListener("click", changePokemon);
